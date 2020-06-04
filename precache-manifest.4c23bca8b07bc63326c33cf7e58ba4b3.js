df858eadf80271eaae908852100101ec90d8e5a9
felcg <xfelipecg@gmail.com>
2020-06-04 17:55:00 -0300

    Updates

---
+++ precache-manifest.4c23bca8b07bc63326c33cf7e58ba4b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1532331a516dd8d09d2a50ec8cc643cb",
+    "url": "/felcg/Portfolio/index.html"
+  },
+  {
+    "revision": "c0f714eda0cbb78bf9e7",
+    "url": "/felcg/Portfolio/static/css/main.f8dd6ebb.chunk.css"
+  },
+  {
+    "revision": "ca34f718a7997d8e2d46",
+    "url": "/felcg/Portfolio/static/js/2.68468765.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/felcg/Portfolio/static/js/2.68468765.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0f714eda0cbb78bf9e7",
+    "url": "/felcg/Portfolio/static/js/main.1310e99a.chunk.js"
+  },
+  {
+    "revision": "3dedf4da958353254484",
+    "url": "/felcg/Portfolio/static/js/runtime-main.e45c3d2d.js"
+  }
+]);
