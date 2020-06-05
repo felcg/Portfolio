2794b0c920e9d2afe74943cbb65d03f22ddb6570
felcg <xfelipecg@gmail.com>
2020-06-05 14:59:03 -0300

    Updated css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ const NavBar = () => (
   <Navbar fixed="top" expand="lg" className="bg-dark sidenav">
     <div className="text-welcome">
       <h1 className="font-weight-bold text-primary">Felipe CG</h1>
-      <p className="text-secondary">Olá, sou um desenvolvedor web com foco em Javascript.
+      <p className="text-light">Olá, sou um desenvolvedor web com foco em Javascript.
         Tenho prática com React, Node.js e Express. Além disso também tenho experiência com
         bancos de dados relacionais e não relacionais. Outras tecnologias que também uso são HTML5,
         CSS e Git.
