1992f6c607f875f5bae26a2c9058a108583a3224
felcg <xfelipecg@gmail.com>
2020-06-05 21:59:19 -0300

    Cria as rotas para Posts

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,10 +2,14 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
 import FrontPage from './FrontPage'
+import Post from './Posts/Post'
+import Posts from './Posts/Posts'
 
 export default () => (
   <Switch>
     <Route exact path="/" component={FrontPage} />
+    <Route exact path="/posts/" component={Posts} />
+    <Route exact path="/posts/:id" component={Post} />
   </Switch>
 
 )
