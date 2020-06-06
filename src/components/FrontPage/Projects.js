80c0607dcee7f20039b6fbc4015483659b0227eb
felcg <xfelipecg@gmail.com>
2020-06-06 17:04:41 -0300

    Adiciona Projeto Portfolio

--- src/components/FrontPage/Projects.js
+++ src/components/FrontPage/Projects.js
@@ -5,6 +5,7 @@ import Project from './Project'
 const Projects = () => (
   <Container className="projects-container">
     <h1>Projetos</h1>
+    <Project Text="React, Sass, Bootstrap, React-Bootstrap, GraphQL, Strapi" Github="https://github.com/felcg/Portfolio" Demo="https://felcg.github.io/Portfolio/" Image="https://res.cloudinary.com/dj641se4s/image/upload/c_scale,w_600/v1591473455/Portfolio.png" />
     <Project Text="React, Sass, Bootstrap, React-Bootstrap" Github="https://github.com/felcg/BootstrapLandingPage" Demo="https://felcg.github.io/BootstrapLandingPage/" Image="https://res.cloudinary.com/dj641se4s/image/upload/w_600/q_100/v1591298728/project1.png" />
     <Project Text="React, Sass, Bootstrap, React-Bootstrap" Github="https://github.com/felcg/BootstrapLandingPage2" Demo="https://felcg.github.io/BootstrapLandingPage2/" Image="https://res.cloudinary.com/dj641se4s/image/upload/w_600/q_auto/v1591298343/project2.png" />
   </Container>
