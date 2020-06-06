a16ce392498c77731c1d9fae76d4b7199990be8f
felcg <xfelipecg@gmail.com>
2020-06-05 21:58:00 -0300

    Adiciona Apollo

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import { ApolloProvider } from 'react-apollo'
+
 
 import './assets/custom.scss'
+import './fontawesome'
 import App from './components/App'
 import ErrorBoundary from './components/ErrorBoundary'
-import './fontawesome'
+import client from './utils/apolloClient'
 
 
 const refresh = () => render(
   <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
+    <ApolloProvider client={client}>
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+    </ApolloProvider>
   </BrowserRouter>,
   document.getElementById('root'),
 )
