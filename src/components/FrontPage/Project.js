e81453a7c7b08ebc8df48d29c70dfba9a08ecd49
felcg <xfelipecg@gmail.com>
2020-07-07 00:25:13 -0300

    Novo layout e adicao de projeto Show do Milhao

--- src/components/FrontPage/Project.js
+++ src/components/FrontPage/Project.js
@@ -3,22 +3,30 @@ import { Card } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Project = ({
-  Text, Github, Demo, Image,
+  Tech, Github, Demo, Image, Sumario, Titulo,
 }) => (
   <Card className="project-card">
-    <Card.Img src={Image} className="project-img" />
-    <div className="card-link-div p-3">
-      <a href={Github}>
-        <FontAwesomeIcon icon={['fab', 'github']} className="icons-img" />
-        Github
-      </a>
-      <a href={Demo}>
-        <FontAwesomeIcon icon={['fas', 'external-link-alt']} className="icons-img" />Demonstração
-      </a>
+    <div>
+      <Card.Img src={Image} className="project-img" />
+      <div className="text-light p-3">
+        <h1>{Titulo}</h1>
+        <p>{Sumario}</p>
+        <div className="card-link-div p-3">
+          <a href={Github} className="mr-3">
+            <FontAwesomeIcon icon={['fab', 'github']} className="icons-img" />
+            Github
+          </a>
+          <a href={Demo}>
+            <FontAwesomeIcon icon={['fas', 'external-link-alt']} className="icons-img" />Demonstração
+          </a>
+        </div>
+        <Card.Text className="font-weight-light">
+          <span className="text-secondary">Tecnologias: </span>{Tech}
+        </Card.Text>
+      </div>
     </div>
-    <Card.Text className="text-white font-weight-light">
-      <span className="text-secondary">Tecnologias: </span>{Text}
-    </Card.Text>
+
+
   </Card>
 )
 
--- src/components/FrontPage/Projects.js
+++ src/components/FrontPage/Projects.js
@@ -5,9 +5,13 @@ import Project from './Project'
 const Projects = () => (
   <Container className="projects-container">
     <h1>Projetos</h1>
-    <Project Text="Express, Redis, Redux, React, Sass, Bootstrap, PostgreSQL" Github="https://github.com/felcg/Gamelogged" Demo="https://felcg.github.io/Gamelogged/" Image="https://res.cloudinary.com/dj641se4s/image/upload/c_scale,q_auto,w_600/v1593504612/gamelogged.png" />
-    <Project Text="React, Sass, Bootstrap, React-Bootstrap" Github="https://github.com/felcg/BootstrapLandingPage" Demo="https://felcg.github.io/BootstrapLandingPage/" Image="https://res.cloudinary.com/dj641se4s/image/upload/w_600/q_100/v1591298728/project1.png" />
-    <Project Text="React, Sass, Bootstrap, React-Bootstrap, GraphQL, Strapi" Github="https://github.com/felcg/Portfolio" Demo="https://felcg.github.io/Portfolio/" Image="https://res.cloudinary.com/dj641se4s/image/upload/c_scale,w_600/v1591473455/Portfolio.png" />
+    <Project Titulo="Gamelogged" Sumario="Site para fazer buscas em banco de dados de jogos eletrônicos. É possível procurar jogos usando filtros ou usando a barra de busca. Nesse projeto usei a API do IGDB.com, site que possui um banco de dados com milhares de jogos." Tech="Express, Redis, Redux, React, Sass, Bootstrap, Heroku" Github="https://github.com/felcg/Gamelogged" Demo="https://felcg.github.io/Gamelogged/" Image="https://res.cloudinary.com/dj641se4s/image/upload/c_scale,q_auto,w_600/v1593504612/gamelogged.png" />
+
+    <Project Titulo="Show do Milhão" Sumario="Jogo inspirado no programa de TV Show do Milhão, exibido pelo SBT em 1997. Possui 300 perguntas, 3 dificuldades, opções para customizar o jogo e ranking local. " Tech="React, Redux, Sass, Bootstrap" Github="https://github.com/felcg/show_do_milhao" Demo="https://felcg.github.io/show_do_milhao/#/" Image="https://res.cloudinary.com/dj641se4s/image/upload/c_scale,q_auto,w_600/v1594082510/show_do_milhao.png" />
+
+    <Project Titulo="Landing Page Personalizada" Sumario="Um template para servir como ponto de partida para criação de uma Landing Page. É fácil de customizar e transformar a página para suprir as necessidades específicas de cada projeto." Tech="React, Sass, Bootstrap" Github="https://github.com/felcg/BootstrapLandingPage" Demo="https://felcg.github.io/BootstrapLandingPage/" Image="https://res.cloudinary.com/dj641se4s/image/upload/w_600/q_100/v1591298728/project1.png" />
+
+    <Project Titulo="Portfolio" Sumario="Esse site que você está! A área de Blogs foi feita usando GraphQL e Strapi." Tech="React, Sass, Bootstrap, GraphQL, Strapi, Heroku" Github="https://github.com/felcg/Portfolio" Demo="https://felcg.github.io/Portfolio/" Image="https://res.cloudinary.com/dj641se4s/image/upload/c_scale,w_600/v1591473455/Portfolio.png" />
   </Container>
 )
 
