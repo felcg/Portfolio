957d9eacfd6097df9c55d55b606f426de24fbcfe
Felipe CG <35982534+felcg@users.noreply.github.com>
2020-07-10 22:18:25 -0300

    Atualiza link para gamelogged

--- src/components/FrontPage/Projects.js
+++ src/components/FrontPage/Projects.js
@@ -5,7 +5,7 @@ import Project from './Project'
 const Projects = () => (
   <Container className="projects-container">
     <h1>Projetos</h1>
-    <Project Titulo="Gamelogged" Sumario="Site para fazer buscas em banco de dados de jogos eletrônicos. É possível procurar jogos usando filtros ou usando a barra de busca. Nesse projeto usei a API do IGDB.com, site que possui um banco de dados com milhares de jogos." Tech="Express, Redis, Redux, React, Sass, Bootstrap, Heroku" Github="https://github.com/felcg/Gamelogged" Demo="https://felcg.github.io/Gamelogged/" Image="https://res.cloudinary.com/dj641se4s/image/upload/c_scale,q_auto,w_600/v1593504612/gamelogged.png" />
+    <Project Titulo="Gamelogged" Sumario="Site para fazer buscas em banco de dados de jogos eletrônicos. É possível procurar jogos usando filtros ou usando a barra de busca. Nesse projeto usei a API do IGDB.com, site que possui um banco de dados com milhares de jogos." Tech="Express, Redis, Redux, React, Sass, Bootstrap, Heroku" Github="https://github.com/felcg/Gamelogged" Demo="https://gamelogged.com" Image="https://res.cloudinary.com/dj641se4s/image/upload/c_scale,q_auto,w_600/v1593504612/gamelogged.png" />
 
     <Project Titulo="Show do Milhão" Sumario="Jogo inspirado no programa de TV Show do Milhão, exibido pelo SBT em 1997. Possui 300 perguntas, 3 dificuldades, opções para customizar o jogo e ranking local. " Tech="React, Redux, Sass, Bootstrap" Github="https://github.com/felcg/show_do_milhao" Demo="https://felcg.github.io/show_do_milhao/#/" Image="https://res.cloudinary.com/dj641se4s/image/upload/c_scale,q_auto,w_600/v1594082510/show_do_milhao.png" />
 
